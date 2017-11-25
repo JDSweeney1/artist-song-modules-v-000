e8d2ad1082a6796e93e77aa65983c7a9bd857bba
Jered Sweeney <sweeneyjered@gmail.com>
2017-11-25 03:19:23 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,6 +4,7 @@ class Artist
 
   extend Memorable
   extend Findable
+  include Paramable
   attr_accessor :name
   attr_reader :songs
 
@@ -31,8 +32,8 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+#  def to_param
+#    name.downcase.gsub(' ', '-')
+#  end
 
 end
